c4e54bb55e2b52c8e4d6a12ecf01ac7f8d3cd7ac
EihabAhmed <eihabahmed@gmail.com>
2023-06-20 16:40:27 +0300

    Initial Commit

---
+++ android/src/com/mygdx/jumpingjackch11/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.jumpingjackch11;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.jumpingjackch11.JumpingJackGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpingJackGame(), config);
+	}
+}
