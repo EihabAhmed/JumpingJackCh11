619a1ac1e46c5a2deeb80f4f95748c0294c94dff
EihabAhmed <eihabahmed@gmail.com>
2023-06-21 06:36:16 +0300

    Added main actor animations and physics

--- core/src/com/mygdx/jumpingjackch11/Koala.java
+++ core/src/com/mygdx/jumpingjackch11/Koala.java
@@ -2,7 +2,9 @@ package com.mygdx.jumpingjackch11;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Koala extends BaseActor {
@@ -14,6 +16,10 @@ public class Koala extends BaseActor {
     private float maxHorizontalSpeed;
     private float gravity;
     private float maxVerticalSpeed;
+
+    private Animation jump;
+    private float jumpSpeed;
+    private BaseActor belowSensor;
 
     public Koala(float x, float y, Stage s) {
         super(x, y, s);
@@ -29,6 +35,15 @@ public class Koala extends BaseActor {
         walkDeceleration = 200;
         gravity = 700;
         maxVerticalSpeed = 1000;
+
+        jump = loadTexture("koala/jump.png");
+        jumpSpeed = 450;
+        setBoundaryPolygon(6);
+        belowSensor = new BaseActor(0, 0, s);
+        belowSensor.loadTexture("white.png");
+        belowSensor.setSize(this.getWidth() - 8, 8);
+        belowSensor.setBoundaryRectangle();
+        belowSensor.setVisible(false);
     }
 
     @Override
@@ -44,5 +59,70 @@ public class Koala extends BaseActor {
         accelerationVec.add(0, -gravity);
 
         velocityVec.add(accelerationVec.x * dt, accelerationVec.y * dt);
+
+        if (!Gdx.input.isKeyPressed(Keys.RIGHT) && !Gdx.input.isKeyPressed(Keys.LEFT)) {
+            float decelerationAmount = walkDeceleration * dt;
+
+            float walkDirection;
+
+            if (velocityVec.x > 0)
+                walkDirection = 1;
+            else
+                walkDirection = -1;
+
+            float walkSpeed = Math.abs(velocityVec.x);
+
+            walkSpeed -= decelerationAmount;
+
+            if (walkSpeed < 0)
+                walkSpeed = 0;
+
+            velocityVec.x = walkSpeed * walkDirection;
+        }
+
+        velocityVec.x = MathUtils.clamp(velocityVec.x, -maxHorizontalSpeed, maxHorizontalSpeed);
+        velocityVec.y = MathUtils.clamp(velocityVec.y, -maxVerticalSpeed, maxVerticalSpeed);
+
+        moveBy(velocityVec.x * dt, velocityVec.y * dt);
+        accelerationVec.set(0, 0);
+
+        belowSensor.setPosition(getX() + 4, getY() - 8);
+
+        alignCamera();
+        boundToWorld();
+
+        if (this.isOnSolid()) {
+            belowSensor.setColor(Color.GREEN);
+
+            if (velocityVec.x == 0)
+                setAnimation(stand);
+            else
+                setAnimation(walk);
+        } else {
+            belowSensor.setColor(Color.RED);
+            setAnimation(jump);
+        }
+
+        if (velocityVec.x > 0) // face right
+            setScaleX(1);
+        if (velocityVec.x < 0) // face left
+            setScaleX(-1);
+    }
+
+    public boolean belowOverlaps(BaseActor actor) {
+        return belowSensor.overlaps(actor);
+    }
+
+    public boolean isOnSolid() {
+        for (BaseActor actor : BaseActor.getList(getStage(), "com.mygdx.jumpingjackch11.Solid")) {
+            Solid solid = (Solid) actor;
+            if (belowOverlaps(solid) && solid.isEnabled())
+                return true;
+        }
+        return false;
+    }
+
+    public void jump() {
+        velocityVec.y = jumpSpeed;
     }
 }
--- core/src/com/mygdx/jumpingjackch11/LevelScreen.java
+++ core/src/com/mygdx/jumpingjackch11/LevelScreen.java
@@ -1,18 +1,13 @@
 package com.mygdx.jumpingjackch11;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-
-import java.awt.image.ImageProducer;
+import com.badlogic.gdx.math.Vector2;
 
 public class LevelScreen extends BaseScreen {
+
+    Koala jack;
 
     @Override
     public void initialize() {
@@ -20,19 +15,44 @@ public class LevelScreen extends BaseScreen {
 
         TilemapActor tma = new TilemapActor("map.tmx", mainStage);
 
-        for (MapObject obj :  tma.getRectangleList("Solid")) {
+        for (MapObject obj : tma.getRectangleList("Solid")) {
             MapProperties props = obj.getProperties();
-            new Solid((float)props.get("x"), (float)props.get("y"),
-                    (float)props.get("width"), (float)props.get("height"), mainStage);
+            new Solid((float) props.get("x"), (float) props.get("y"),
+                    (float) props.get("width"), (float) props.get("height"), mainStage);
         }
+
+        MapObject startPoint = tma.getRectangleList("start").get(0);
+        MapProperties startProps = startPoint.getProperties();
+        jack = new Koala((float) startProps.get("x"), (float) startProps.get("y"), mainStage);
     }
 
     @Override
     public void update(float dt) {
+        for (BaseActor actor : BaseActor.getList(mainStage, "com.mygdx.jumpingjackch11.Solid")) {
+            Solid solid = (Solid) actor;
+
+            if (jack.overlaps(solid) && solid.isEnabled()) {
+                Vector2 offset = jack.preventOverlap(solid);
+
+                if (offset != null) {
+                    // collided in X direction
+                    if (Math.abs(offset.x) > Math.abs(offset.y))
+                        jack.velocityVec.x = 0;
+                    else // collided in Y direction
+                        jack.velocityVec.y = 0;
+                }
+            }
+        }
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public boolean keyDown(int keycode) {
+        if (keycode == Keys.SPACE) {
+            if (jack.isOnSolid()) {
+                jack.jump();
+            }
+        }
+
         return false;
     }
 }
