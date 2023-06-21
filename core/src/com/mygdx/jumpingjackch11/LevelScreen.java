f9e1eeb17b6f6be627eb01244af23a879b9ccafc
EihabAhmed <eihabahmed@gmail.com>
2023-06-21 09:19:24 +0300

    Added target flag

--- core/src/com/mygdx/jumpingjackch11/LevelScreen.java
+++ core/src/com/mygdx/jumpingjackch11/LevelScreen.java
@@ -4,10 +4,21 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class LevelScreen extends BaseScreen {
 
     Koala jack;
+
+    boolean gameOver;
+    int coins;
+    float time;
+    Label coinLabel;
+    Table keyTable;
+    Label timeLabel;
+    Label messageLabel;
 
     @Override
     public void initialize() {
@@ -24,10 +35,47 @@ public class LevelScreen extends BaseScreen {
         MapObject startPoint = tma.getRectangleList("start").get(0);
         MapProperties startProps = startPoint.getProperties();
         jack = new Koala((float) startProps.get("x"), (float) startProps.get("y"), mainStage);
+
+        gameOver = false;
+        coins = 0;
+        time = 60;
+        coinLabel = new Label("Coins: " + coins, BaseGame.labelStyle);
+        coinLabel.setColor(Color.GOLD);
+        keyTable = new Table();
+        timeLabel = new Label("Time: " + (int)time, BaseGame.labelStyle);
+        timeLabel.setColor(Color.LIGHT_GRAY);
+        messageLabel = new Label("Message", BaseGame.labelStyle);
+        messageLabel.setVisible(false);
+
+        uiTable.pad(20);
+        uiTable.add(coinLabel);
+        uiTable.add(keyTable).expandX();
+        uiTable.add(timeLabel);
+        uiTable.row();
+        uiTable.add(messageLabel).colspan(3).expandY();
+
+        for (MapObject obj : tma.getTileList("Flag")) {
+            MapProperties props = obj.getProperties();
+            new Flag((float)props.get("x"), (float)props.get("y"), mainStage);
+        }
     }
 
     @Override
     public void update(float dt) {
+
+        if (gameOver)
+            return;
+
+        for (BaseActor flag : BaseActor.getList(mainStage, "com.mygdx.jumpingjackch11.Flag")) {
+            if (jack.overlaps(flag)) {
+                messageLabel.setText("You Win!");
+                messageLabel.setColor(Color.LIME);
+                messageLabel.setVisible(true);
+                jack.remove();
+                gameOver = true;
+            }
+        }
+
         for (BaseActor actor : BaseActor.getList(mainStage, "com.mygdx.jumpingjackch11.Solid")) {
             Solid solid = (Solid) actor;
 
